9e74a75e310180075d3b0aaa13e76e0ca342d0d5
leonTrujillo <101562445+leonTrujillo@users.noreply.github.com>
2022-04-19 10:32:12 +0200

    Delete FragmentPresentadorInterface.java

--- presentador/FragmentPresentadorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.coursera.semana3.mascotas1.presentador;
-
-public interface FragmentPresentadorInterface {
-
-    public void obtenMascotasBaseDatos();
-
-    public void mostrarMascotasEnRecyclerView();
-
-}
