9986566aac5186235782ca1fa7eb65c629220368
leonTrujillo <101562445+leonTrujillo@users.noreply.github.com>
2022-04-19 10:31:44 +0200

    Delete ContraYmail.java

--- pojo/ContraYmail.java
+++
@@ -1,6 +0,0 @@
-package com.coursera.semana3.mascotas1.pojo;
-
-public class ContraYmail {
-    public static String mail="aycomosufro@hotmail.com";
-    public static String contra="Poyastron79";
-}
